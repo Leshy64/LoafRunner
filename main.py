3dd03c8cdf0d17e7e5900fbbabcbb4bb52210e52
Leshy64 <roma.kolodeev@mail.ru>
2026-06-07 09:57:25 +0500

    Added moving background.

--- main.py
+++ main.py
@@ -5,6 +5,26 @@ screen = pygame.display.set_mode((1000, 800))
 
 isRunning = True
 
+# Background
+bg = pygame.image.load("res/bg1.png")
+bg = pygame.transform.scale(bg, (1000, 570))
+road = pygame.image.load("res/road.png")
+road = pygame.transform.scale(road, (1000, 230))
+bgX: float = 0
+bgSpeed: float = 20
+
+
+def moveBackground() -> None:
+    global bgX
+    bgX -= bgSpeed
+    _ = screen.blit(bg, (bgX, 0))
+    _ = screen.blit(bg, (bgX + 1000, 0))
+    _ = screen.blit(road, (bgX, 570))
+    _ = screen.blit(road, (bgX + 1000, 570))
+    if bgX == -1000:
+        bgX = 0
+
+
 # Player
 loafX: float = 100.0
 loafY: float = 500.0
@@ -54,7 +74,7 @@ def movePlayer(y: float) -> None:
 
 clock = pygame.time.Clock()
 while isRunning:
-    _ = screen.fill((0, 0, 0))
+    # _ = screen.fill((0, 0, 0))
     dt_ms = clock.tick(32)
     dt = dt_ms / 1000.0
 
@@ -65,5 +85,6 @@ while isRunning:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and isOnGround(loafY):
             velocity = speed * dt
+    moveBackground()
     movePlayer(loafY)
     pygame.display.flip()
